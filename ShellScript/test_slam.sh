3c9f272f2bfff13b0ead31f582cbb14d843f28c2
Roy Veshovda <roy@veshovda.no>
2018-06-13 16:39:13 +0000

    Wall following

--- ShellScript/test_slam.sh
+++ ShellScript/test_slam.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-catkin_ws=/home/workspace/catkin_ws
-source ${catkin_ws}/devel/setup.bash
+source /home/workspace/catkin_ws/devel/setup.bash
 
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${catkin_ws}/src/home_service/Worlds/udacity.world" &
+xterm -e roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/home_service/Worlds/udacity.world &
+sleep 10
+xterm -e roslaunch turtlebot_gazebo gmapping_demo.launch &
 sleep 5
-xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
+xterm -e roslaunch turtlebot_rviz_launchers view_navigation.launch &
 sleep 5
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-sleep 5
-xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch" &
+xterm -e roslaunch turtlebot_teleop keyboard_teleop.launch &
 sleep 5
--- ShellScript/wall_follower.sh
+++ ShellScript/wall_follower.sh
@@ -4,7 +4,8 @@ source ${catkin_ws}/devel/setup.bash
 
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${catkin_ws}/src/home_service/Worlds/udacity.world" &
 sleep 5
-xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch custom_gmapping_launch_file:=${catkin_ws_dir}/src/home_service/wall_follower/launch/include/gmapping.launch.xml" &
+#xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch custom_gmapping_launch_file:=${catkin_ws_dir}/src/home_service/wall_follower/launch/include/gmapping.launch.xml" &
+xterm -e "roslaunch home_service gmapping.launch" &
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
